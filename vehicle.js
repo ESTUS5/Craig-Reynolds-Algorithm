69a9877aeb2a7d5323434fc346fcfb2a8c505977
Maksymilian Klimczak <34924553+ESTUS5@users.noreply.github.com>
2018-10-19 20:43:46 +0200

    Add files via upload

--- vehicle.js
+++ vehicle.js
@@ -4,11 +4,10 @@ var Vehicle = function(x,y) {
     this.position = [x,y];
     this.mass = 5;
     this.radius = this.mass * 2;
-    this.maxspeed = 2 * 1 / this.mass;
+    this.maxspeed = 0.5 * 1 / this.mass;
     this.maxforce = 1 / (this.mass * this.maxspeed);
 
     this.update = function() {
-        
         this.velocity[0] += this.acceleration[0];
         this.velocity[1] += this.acceleration[1];
         this.position[0] += this.velocity[0];
@@ -26,24 +25,21 @@ var Vehicle = function(x,y) {
         targetX = mouseObj.x;
         targetY = mouseObj.y;
         mouseObj.Init("#VP");
-
         var desired_velocity = [];
         desired_velocity[0] = (targetX - this.position[0]) * this.maxspeed;
         desired_velocity[1] = (targetY - this.position[1]) * this.maxspeed;
-
         var steering_force = [];
         steering_force[0] = desired_velocity[0] - this.velocity[0];
         steering_force[1] = desired_velocity[1] - this.velocity[1];
         steering_force[0] = steering_force[0] / this.mass;
-        steering_force[1] = steering_force[1] / this.mass;
-        
+        steering_force[1] = steering_force[1] / this.mass;      
         this.applyForce(steering_force);
     };
 
     this.render = function() {
-        //var theta = this.velocity + 3.14 / 2;
+        var theta = Math.atan2(this.velocity[1],this.velocity[0]) + Math.PI / 2;
         ctx.translate(this.position[0],this.position[1]);
-        //ctx.rotate(theta);
+        ctx.rotate(theta);
         ctx.beginPath();
             ctx.moveTo(0,-this.radius * 2);
             ctx.lineTo(-this.radius, this.radius * 2);
