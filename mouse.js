85a15ac368d34f35f0e671b932b0aedb20da616b
Maksymilian Klimczak <34924553+ESTUS5@users.noreply.github.com>
2018-12-03 23:01:03 +0100

    Add files via upload

--- mouse.js
+++ mouse.js
@@ -1,13 +1,13 @@
 var Mouse = function()
 {
-    this.x = 100;
-    this.y = 100;
+    this.x = 250;
+    this.y = 250;
     var that = this;
     this.Init = function(element)
     {
         $(element).on("mousemove", function(event){
             that.x = event.pageX;
-            that.y = event.pageY;
+            that.y = event.pageY - 25;
         });
     };
 };
--- vehicle.js
+++ vehicle.js
@@ -1,58 +1,140 @@
-var Vehicle = function(x,y) {
+function Vehicle(x,y) {
     this.acceleration = [0,0];
     this.velocity = [0,0];
     this.position = [x,y];
-    this.mass = 5;
-    this.radius = this.mass * 2;
-    this.maxspeed = 0.5 * 1 / this.mass;
-    this.maxforce = 1 / (this.mass * this.maxspeed);
-
-    this.update = function() {
-        this.velocity[0] += this.acceleration[0];
-        this.velocity[1] += this.acceleration[1];
+   // this.mass = 1;
+    this.radius = 3;//this.mass * 2;
+    this.maxspeed = 4;//0.05 * 1 / this.mass;
+    this.maxforce = 1;//1 / 4*(this.mass * this.maxspeed);
+    this.desired_velocity = [0,0];
+    this.steering_force = [0,0];
+}
+    Vehicle.prototype.normalize = function(variable){
+        if(variable < 0){ 
+            variable = variable * -1;
+        }
+        return variable;
+    }
+    Vehicle.prototype.update = function() {
+        if(this.normalize(this.acceleration[0])>this.maxspeed){
+            this.velocity[0] += (this.maxspeed * this.acceleration[0] / this.normalize(this.acceleration[0]) );
+        }
+        else {
+            this.velocity[0] += this.acceleration[0];
+        }
+        if(this.normalize(this.acceleration[1])>this.maxspeed){
+            this.velocity[1] += (this.maxspeed * this.acceleration[1] / this.normalize(this.acceleration[1]) );
+        }
+        else {
+            this.velocity[1] += this.acceleration[1];
+        }
         this.position[0] += this.velocity[0];
         this.position[1] += this.velocity[1];
         this.acceleration[0] = 0;
         this.acceleration[1] = 0;
-    };
+    }
+    Vehicle.prototype.applyForce = function(force) {
+        if(this.normalize(force[0])>this.maxforce){
+            this.acceleration[0] += (this.maxforce * force[0] / this.normalize(force[0]) );
+        }
+        else {
+            this.acceleration[0] += force[0];
+        }
+        if(this.normalize(force[1])>this.maxforce){
+            this.acceleration[1] += (this.maxforce * force[1] / this.normalize(force[1]) );
+        }
+        else {
+            this.acceleration[1] += force[1];
+        }
+    }
+    Vehicle.prototype.seek = function() {
+        var targetX = mouseObj.x;
+        var targetY = mouseObj.y
+        this.desired_velocity[0] = (targetX - this.position[0]) * this.maxspeed;
+        this.desired_velocity[1] = (targetY - this.position[1]) * this.maxspeed;
+        this.steering_force[0] = this.desired_velocity[0] - this.velocity[0];
+        this.steering_force[1] = this.desired_velocity[1] - this.velocity[1];
+        this.steering_force[0] = this.steering_force[0] ;/// this.mass;
+        this.steering_force[1] = this.steering_force[1] ;/// this.mass;      
+        this.applyForce(this.steering_force);
+    }
+    Vehicle.prototype.flee = function() {
+        var chaserX = mouseObj.x;
+        var chaserY = mouseObj.y
+        this.desired_velocity[0] = (chaserX + this.position[0]) * this.maxspeed;
+        this.desired_velocity[1] = (chaserY + this.position[1]) * this.maxspeed;
+        this.steering_force[0] = this.desired_velocity[0] - this.velocity[0];
+        this.steering_force[1] = this.desired_velocity[1] - this.velocity[1];
+        this.steering_force[0] = this.steering_force[0] ;/// this.mass;
+        this.steering_force[1] = this.steering_force[1] ;/// this.mass;      
+        this.applyForce(this.steering_force);
+    }
+    Vehicle.prototype.map = function(input_val,range_min,range_max){
+        return range_min + (range_max-range_min) * ((input_val - 0) / (100 - 0));
+    }
+    Vehicle.prototype.arrival = function() {
+        var targetX = mouseObj.x;
+        var targetY = mouseObj.y
+        this.desired_velocity[0] = (targetX - this.position[0]);
+        this.desired_velocity[1] = (targetY - this.position[1]);
 
-    this.applyForce = function(force) {
-        this.acceleration[0] += force[0];
-        this.acceleration[1] += force[1];
-    };
+        if(this.normalize(this.desired_velocity[0]) <= 10 && this.normalize(this.desired_velocity[1]) <= 10){
+            this.desired_velocity[0] = this.desired_velocity[0] * this.map(this.desired_velocity[0],0,10);
+            this.desired_velocity[1] = this.desired_velocity[1] * this.map(this.desired_velocity[1],0,10);
+        }
 
-    this.seek = function() {
-        targetX = mouseObj.x;
-        targetY = mouseObj.y;
-        mouseObj.Init("#VP");
-        var desired_velocity = [];
-        desired_velocity[0] = (targetX - this.position[0]) * this.maxspeed;
-        desired_velocity[1] = (targetY - this.position[1]) * this.maxspeed;
-        var steering_force = [];
-        steering_force[0] = desired_velocity[0] - this.velocity[0];
-        steering_force[1] = desired_velocity[1] - this.velocity[1];
-        steering_force[0] = steering_force[0] / this.mass;
-        steering_force[1] = steering_force[1] / this.mass;      
-        this.applyForce(steering_force);
-    };
-
-    this.render = function() {
+        this.steering_force[0] = this.desired_velocity[0] - this.velocity[0];
+        this.steering_force[1] = this.desired_velocity[1] - this.velocity[1];
+        this.steering_force[0] = this.steering_force[0] / this.mass;
+        this.steering_force[1] = this.steering_force[1] / this.mass;      
+        this.applyForce(this.steering_force);
+    }
+    Vehicle.prototype.renderPathFromVehicleToPosXY = function(x,y){
+        this.ctx = canvas.getContext("2d");
+        this.ctx.beginPath();
+            this.ctx.moveTo(this.position[0],this.position[1]);
+            this.ctx.lineTo(x,y);
+            this.ctx.strokeStyle = "rgba(255,0,0,0.5)";
+            this.ctx.stroke();
+        this.ctx.closePath();
+    }
+    Vehicle.prototype.renderCircleAroundPosXY = function(x,y,r = this.radius){
+        this.ctx = canvas.getContext("2d");
+        this.ctx.beginPath();
+            this.ctx.arc(x,y,r * 2.2,0,2*Math.PI);
+            this.ctx.strokeStyle = "rgba(255, 0, 255,0.5)";
+            this.ctx.stroke();
+        this.ctx.closePath();
+    }
+    Vehicle.prototype.renderVehicle = function() {
+        this.ctx = canvas.getContext("2d");
+        //operation to prevent invalid drawings
+        this.ctx.save();
+        //vehicle
         var theta = Math.atan2(this.velocity[1],this.velocity[0]) + Math.PI / 2;
-        ctx.translate(this.position[0],this.position[1]);
-        ctx.rotate(theta);
-        ctx.beginPath();
-            ctx.moveTo(0,-this.radius * 2);
-            ctx.lineTo(-this.radius, this.radius * 2);
-            ctx.lineTo(this.radius, this.radius * 2);
-            ctx.closePath();
-            ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-            ctx.stroke();
-        ctx.closePath();
-    };
-    
-    this.loop = function() {
+        this.ctx.translate(this.position[0],this.position[1]);
+        this.ctx.rotate(theta);
+        this.ctx.beginPath();
+            this.ctx.moveTo(0,-this.radius * 2);
+            this.ctx.lineTo(-this.radius, this.radius * 2);
+            this.ctx.lineTo(this.radius, this.radius * 2);
+            this.ctx.closePath();
+            this.ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+            this.ctx.stroke();
+        this.ctx.closePath();
+        //we restore position of (0,0) from save
+        this.ctx.restore();
+    }
+    Vehicle.prototype.render = function() {
+        this.renderPathFromVehicleToPosXY(mouseObj.x,mouseObj.y);
+        this.renderCircleAroundPosXY(this.position[0],this.position[1]);
+        this.renderVehicle();
+        this.renderCircleAroundPosXY(mouseObj.x,mouseObj.y,10);
+    }
+    Vehicle.prototype.run = function(){
         this.render();
         this.seek();
+        //this.flee();
+        //this.arrival();
         this.update();
-    };
-};
+    }
